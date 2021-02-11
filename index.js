6981e43cb1e09c6e5c28f0f542c99137a1b2c9d7
David Zirima <davidtzirima@gmail.com>
2021-02-11 12:59:51 +0200

    correected url

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ const PORT = process.env.PORT;
 const url = "mongodb://localhost:27017/FansetTechinicians"; // for local dev
 const url2 = process.env.CONNECTION_UR
 
-mongoose.connect(url2, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 .then(() => app.listen(PORT, () => console.log(`server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message))
+
 
 mongoose.set('useFindAndModify', false);
